3e51ddab985598f7c256a534d2f47df8bf1b0ce4
neupanesaugat <saugatneupane50@gmail.com>
2024-07-27 21:00:44 +0545

    instagram api using express js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import connectDB from "./database-connection/connect-db.js";
 import userRoutes from "./user/user.controller.js";
+import postRoutes from "./user-post/post.controller.js";
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(express.json());
 
 // routes
 app.use("/user", userRoutes);
+app.use("/post", postRoutes);
 
 // DB connection
 await connectDB();
--- user-post/post.model.js
+++ user-post/post.model.js
@@ -2,9 +2,27 @@ import mongoose from "mongoose";
 
 // set schema
 const postSchema = mongoose.Schema({
-  description: String,
-  location: String,
-  image: String,
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 100,
+  },
+  location: {
+    type: String,
+    required: false,
+    trim: true,
+    maxlength: 50,
+  },
+  image: {
+    type: String,
+    required: false,
+  },
+  userId: {
+    type: mongoose.ObjectId,
+    ref: "User",
+    required: true,
+  },
 });
 
 // create model
